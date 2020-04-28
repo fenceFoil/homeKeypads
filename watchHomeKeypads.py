74f871e3481a549be254bccb227079735eb0fb6f
fenceFoil <karnavasbj@gmail.com>
2020-04-28 01:50:06 -0400

    Switching away from curses again

--- watchHomeKeypads.py
+++ watchHomeKeypads.py
@@ -1,26 +1,5 @@
-import curses
-import os
 import subprocess
 
-PROMPT = "watchHomeKeypads.py running. Press Ctrl+C to exit."
-
-def main(win):
-	win.nodelay(True)
-	key=""
-	win.clear()
-	win.addstr(PROMPT)
-	while 1:
-		try:
-			key = win.getkey()
-			win.clrtoeol()
-			win.addstr(PROMPT)
-			win.addstr(str(key))
-			if key == os.linesep:
-				break
-			else:
-				subprocess.run(["aplay", "homeKeypads/sounds/key.wav"])
-		except Exception as e:
-			# No input
-			pass
-
-curses.wrapper(main)
+while True:
+    inLine = input("watchHomeKeypads.py running. Press Ctrl+C to exit.")
+    subprocess.run(["aplay", "homeKeypads/sounds/key.wav"])
